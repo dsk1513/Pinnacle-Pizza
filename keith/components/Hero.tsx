4129d4d95e303e809d221a2e7928ad8e131fda54
dsk1513 <dc697@njit.edu>
2023-12-13 12:12:03 -0500

    removed the  ordernow button

--- keith/components/Hero.tsx
+++ keith/components/Hero.tsx
@@ -13,8 +13,7 @@ const Hero = ({ backgroundImage, title, subtitle, onOrderNowClick }) => {
         <div className={styles.content}>
           <h1>{title}</h1>
           <p>{subtitle}</p>
-          {/* When this button is clicked, it will call the function passed from the parent */}
-          <button className="orangeButton" onClick={onOrderNowClick}>Order Now</button>
+          
         </div>
       </div>
     </div>
