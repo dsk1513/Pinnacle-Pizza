0d248246cc8b58c006333505837f82489c55bdb0
dsk1513 <dc697@njit.edu>
2023-12-13 21:51:35 -0500

    update

--- keith/app/page.tsx
+++ keith/app/page.tsx
@@ -1,36 +1,23 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import '/Users/dineshchalla/Desktop/keith/group-project/keith/styles/globals.css'; // Import the CSS file
 import Cards from "@/components/Cards";
 import AboutUs from "@/components/AboutUs";
-
-
+import '/Users/dineshchalla/Desktop/keith/group-project/keith/styles/globals.css'; // Adjust this path
 
 export default function Home() {
-    return (
-        <div className="header-top">
-            <Header />
-            <br />
-            <Hero
-        backgroundImage="/images/PIZZA.jpeg"
-        title={<><span>HOT AND CHEESY</span><br /><span>PIZZA</span></>}
-        subtitle="50% OFF 🌟🍕"
-      />
-      <br />
-      <br />
-      <br />
-      <br /><br /><br />
+  return (
+    <div className="header-top">
+      <Header />
 
-      <Cards/>
-      <br /><br /> <br /><br />
-            <AboutUs/>
-        </div>
-    );
+      <Hero
+        backgroundImage="/images/heroblack.png"
+        
+        title={<><span>HOT AND CHEESY</span><br /><span>PIZZA</span></>}
+        subtitle="50% OFF on your first order 🌟🍕"
+      />
+      <Cards />
+      <AboutUs />
+    </div>
+  );
 }
--- keith/components/Hero.tsx
+++ keith/components/Hero.tsx
@@ -1,19 +1,16 @@
-// components/Hero.js
 import React from "react";
-import styles from "./Hero.module.css"; // Import the module styles
+import styles from "./hero.module.css"; // Adjust the path to your CSS file
 
-const Hero = ({ backgroundImage, title, subtitle, onOrderNowClick }) => {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-  };
-
+const Hero = ({ backgroundImage, title, subtitle }) => {
   return (
-    <div className={styles.hero} style={backgroundStyle}>
-      <div className={styles.overlay}>
+    <div className={styles.hero}>
+      <div className={styles.heroLeft}>
+        <img src={backgroundImage} alt="Hero" />
+      </div>
+      <div className={styles.heroRight}>
         <div className={styles.content}>
           <h1>{title}</h1>
           <p>{subtitle}</p>
-          
         </div>
       </div>
     </div>
