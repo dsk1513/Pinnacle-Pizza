f63aa758f827c202e0889aa05b6284cff658b709
dsk1513 <dc697@njit.edu>
2023-12-14 18:17:16 -0500

    new12

--- keith/components/Hero.tsx
+++ keith/components/Hero.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./hero.module.css"; // Adjust the path to your CSS file
 
-const Hero = ({ backgroundImage, title, subtitle }) => {
+interface HeroProps {
+  backgroundImage: string;
+  title: React.ReactNode;
+  subtitle: string;
+}
+
+const Hero: React.FC<HeroProps> = ({ backgroundImage, title, subtitle }) => {
   return (
     <div className={styles.hero}>
       <div className={styles.heroLeft}>
-        <img src={backgroundImage} alt="Hero" />
+        {/* Replace with Image component and provide width and height or a layout property */}
+        <Image src={backgroundImage} alt="Hero" layout="fill" objectFit="cover" />
       </div>
       <div className={styles.heroRight}>
         <div className={styles.content}>
