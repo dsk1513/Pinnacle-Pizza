c2482f30641e956c3c9e65d60456c236b167f799
dsk1513 <dc697@njit.edu>
2023-12-14 19:11:07 -0500

    gtag

--- keith/app/page.tsx
+++ keith/app/page.tsx
@@ -4,10 +4,24 @@ import Hero from "@/components/Hero";
 import Cards from "@/components/Cards";
 import Aboutuss from "@/components/Aboutuss";
 import "../styles/globals.css"
+import Script from "next/script";
 export default function Home() {
   return (
+<div>
+   <Script 
+    strategy="afterInteractive"
+   src="https://www.googlegletagmanager.com/gtag/jhs?id=G-7RJ25106X0"/>
+   <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || []:
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js','G-7RJ25106X0');
+
+    `}
+   </Script>
     <div className="header-top">
-      <Header />
+      <Header /></div> 
+
       <Hero
         backgroundImage="/images/heroblack.png"
         title={<><span>HOT AND CHEESY</span><br /><span>PIZZA</span></>}
