aa5203d4b4425c1fbc86007454249667560cd282
dsk1513 <dc697@njit.edu>
2023-12-15 22:18:57 -0500

    google analytics

--- keith/app/page.tsx
+++ keith/app/page.tsx
@@ -4,22 +4,10 @@ import Hero from "@/components/Hero";
 import Cards from "@/components/Cards";
 import Aboutuss from "@/components/Aboutuss";
 import "../styles/globals.css"
-import Script from "next/script";
+
 export default function Home() {
   return (
 <div>
-   <Script 
-    strategy="afterInteractive"
-   src="https://www.googlegletagmanager.com/gtag/jhs?id=G-7RJ25106X0"/>
-   <Script id="google-analytics" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || []:
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config','G-7RJ25106X0');
-
-    `}
-   </Script>
     <div className="header-top">
       <Header /></div> 
 
