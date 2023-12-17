222e05c272fcb4942a68d04d36911d59892bfd8c
dsk1513 <dc697@njit.edu>
2023-12-17 17:48:25 -0500

    page

--- keith/app/page.tsx
+++ keith/app/page.tsx
@@ -1,23 +1,30 @@
-import React from "react";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+
 import Cards from "@/components/Cards";
-import Aboutuss from "@/components/Aboutuss";
-import "../styles/globals.css"
-
+import About from "@/components/Aboutuss";
+import Cardstext from "@/components/Cardstext";
+import Footer from "@/components/Footer";
 export default function Home() {
-  return (
-<div>
-    <div className="header-top">
-      <Header /></div> 
-
-      <Hero
-        backgroundImage="/images/heroblack.png"
-        title={<><span>HOT AND CHEESY</span><br /><span>PIZZA</span></>}
-        subtitle="50% OFF on your first order 🌟🍕"
-      />
-      <Cards />
-      <Aboutuss/>
-    </div>
-  );
+	return (
+		<>
+	
+		
+		<br /><br /><br /><br /><br /><br />
+    <Cardstext/>
+    <br /><br />
+		<Cards/>
+		<br /><br /><br /><br /><br /><br /><br /><br></br>
+		 <About/>
+		 <br /><br /><br />
+		 <Footer/>
+		</>
+		
+	);
 }
